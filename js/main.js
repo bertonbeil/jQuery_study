bf98850b922a1cc10271449cb42b4010ccf3471c
Andrew Dmytriv <skateboardsmart@gmail.com>
2016-08-09 15:07:32 +0300

    add callback function on click event early set name for function

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
-$(document).ready(function(){
-  $('.imgs').on('click', function(){
-    var discount = Math.floor( (Math.random()*5) + 5 ),
-        msg = '<p>Your discount ' + discount + '%</p>';
-      $(this).append(msg);
+$(document).ready(function() {
+    $('.imgs').click(checkForCode);
 
-  });
+    function checkForCode () {
+        var discount = Math.floor((Math.random() * 5) + 5),
+            msg = '<p>Your discount ' + discount + '%</p>';
+
+        $(this).append(msg);
+
+        $('.imgs').each(function () {
+            $(this).off('click');
+        });
+    }
+    
 });
